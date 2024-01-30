8f6aca13dd8d5dc44a38540dfb544736b03b5919
Spoon <beaucamps10@gmail.com>
2024-01-31 09:53:55 +1100

    New cyberbar script, added NUI back

--- cyberbar/config.lua
+++ cyberbar/config.lua
@@ -1,5 +1,7 @@
 Config = {}
 
+Config.arenaPos = vector3(2800.123, -3698.778, 140.001)
+
 Config.arenaOptions = {
     dystopian = {
         scene = {
@@ -60,4 +62,42 @@ Config.arenaOptions = {
             'EnableInteriorProp(GetInteriorAtCoords(2800.000, -3800.000, 100.000), "Set_Wasteland_10")'
         }
     }
+}
+
+Config.seatMarkers = {
+    {seat = seatOne, team = 'Alpha', status = false, coords = vector3(325.3435, -904.7751, 29.25145)},
+    {seat = seatTwo, team = 'Alpha', status = false, coords = vector3(327.0339, -904.6738, 29.25145)},
+    {seat = seatThree, team = 'Alpha',status = false, coords = vector3(328.7007, -904.6966, 29.25145)},
+    {seat = seatFour, team = 'Alpha', status = false, coords = vector3(330.4021, -904.6966, 29.25145)},
+    {seat = seatFive, team = 'Alpha', status = false, coords = vector3(332.0874, -904.6988, 29.25145)},
+    {seat = seatSix, team = 'Beta', status = false, coords = vector3(337.5286, -903.5734, 29.25146)},
+    {seat = seatSeven, team = 'Beta', status = false, coords = vector3(339.2241, -903.5478, 29.25146)},
+    {seat = seatEight, team = 'Beta', status = false, coords = vector3(340.9525, -903.3763, 29.25146)},
+    {seat = seatNine, team = 'Beta', status = false, coords = vector3(342.65, -903.535, 29.25146)},
+    {seat = seatTen, team = 'Beta', status = false, coords= vector3(344.2519, -903.4109, 29.25146)}
+}
+
+Config.weaponOptions = {
+    'weapon_pistol_mk2',
+    'weapon_appistol',
+    'weapon_pistol50',
+    'weapon_microsmg',
+    'weapon_smg',
+    'weapon_smg_mk2',
+    'weapon_assaultsmg',
+    'weapon_machinepistol',
+    'weapon_minismg',
+    'weapon_assaultrifle',
+    'weapon_assaultrifle_mk2',
+    'weapon_carbinerifle',
+    'weapon_carbinerifle_mk2',
+    'weapon_advancedrifle',
+    'weapon_specialcarbine',
+    'weapon_specialcarbine_mk2',
+    'weapon_bullpuprifle',
+    'weapon_bullpuprifle_mk2',
+    'weapon_compactrifle',
+    'weapon_mg',
+    'weapon_combatmg',
+    'weapon_combatmg_mk2',
 }
